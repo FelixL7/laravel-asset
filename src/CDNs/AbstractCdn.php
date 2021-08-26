ea4cd8079a57ccefdb6dc5c48ea370efc280b9d6
Felix Lipinski <lipinski.felix@ddv-mediengruppe.de>
2021-08-26 16:42:14 +0200

    Secured AbstractCdn:getFilePathWithoutExtension(), URL String Generation with parts as array

--- src/CDNs/AbstractCdn.php
+++ src/CDNs/AbstractCdn.php
@@ -16,7 +16,30 @@ abstract class AbstractCDN
     }
 
     public function getFilePathWithoutExtension($filePath = "/") : string {
-        return "{$this->getDomain()}{$this->getLibPath()}/{$this->libPathName}@{$this->version}{$filePath}/{$this->fileName}";
+        $urlParts = [$this->getDomain(), $this->getLibPath(), $this->libPathName, $this->version, $filePath, $this->fileName];
+        $urlString = "";
+
+        foreach ($urlParts as $key => $urlPart) {
+            $part = trim($urlPart, "/");
+
+            if(empty($part)) {
+                continue;
+            }
+
+            $urlString .= $this->getFilePathPart($part, $key);
+        }
+
+        return $urlString;
+    }
+
+    protected function getFilePathPart($part, $key) {
+        switch ($key) {
+            case 0:
+                return $part;
+
+            default:
+                return "/{$part}";
+        }
     }
 
     public abstract function getDomain() : string;
